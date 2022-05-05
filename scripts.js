2e0c79547d6994ca64514b01d5761954c3901939
DyegoIthalo <dyegoithalo11@hotmail.com>
2022-05-05 12:29:02 -0300

    criação da função para acender próxima cor, checar botões e para o clique do usuário.

--- scripts.js
+++ scripts.js
@@ -12,6 +12,7 @@ const red = document.querySelector(".red");
 const green = document.querySelector(".green");
 const yellow = document.querySelector(".yellow");
 
+//Cria ordem aleatória de cores
 let shuffleOrder = () => {
   let colorOrder = Math.floor(Math.random() * 4);
   order[order.length] = colorOrder;
@@ -21,4 +22,41 @@ let shuffleOrder = () => {
     let elementColor = createColorElement(order[i]);
     lightColor(elementColor, Number(i) + 1);
   }
+};
+
+//acende a próxima cor
+let lightColor = (element, number) => {
+  number = number * 500;
+  setTimeout(() => {
+    element.classList.add("selected");
+  }, number - 250);
+  setTimeout(() => {
+    element.classList.remove("selected");
+  }, number + 200);
+};
+
+//Checa se os botões clicados são os mesmos da ordem gerada no jogo
+let checkOrder = () => {
+  for (let i in clickedOrder) {
+    if (clickedOrder[i] != order[i]) {
+      lose();
+      break;
+    }
+  }
+  if (clickedOrder.length == order.length) {
+    alert("Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!");
+    nextLevel();
+  }
+};
+
+//função para o clique do usuário
+let click = (color) => {
+  clickedOrder[clickedOrder.length] = color;
+  createColorElement(color).classList.add("selected");
+
+  setTimeout(() => {
+    createColorElement(color).classList.remove("selected");
+  });
+
+  checkOrder();
 };
